46a0b9e0b353a6f537c22bb0cdfd21216398e676
tree-townhouse <tree@townhouse.work>
2026-02-24 23:19:16 +0900

    feat: Introduce centralized navbar item definitions and an instance overview page.

--- packages/frontend/src/pages/about.overview.vue
+++ packages/frontend/src/pages/about.overview.vue
@@ -92,14 +92,6 @@ SPDX-License-Identifier: AGPL-3.0-only
 					<template #icon><i class="ti ti-message"></i></template>
 					<template #default>{{ i18n.ts.feedback }}</template>
 				</FormLink>
-				<button :class="$style.main" class="_button" @click="donateCherryPick">
-					<span :class="$style.icon"><i class="ti ti-pig-money"></i></span>
-					<!-- <span :class="$style.text">{{ i18n.tsx.supportThisInstance({ name: instance.name ?? host }) }}</span> -->
-					<span :class="$style.text">{{ i18n.tsx.supportThisInstance({ name: 'CherryPick' }) }}</span>
-					<span :class="$style.suffix">
-						<i class="ti ti-external-link"></i>
-					</span>
-				</button>
 			</div>
 		</div>
 	</FormSection>
@@ -146,7 +138,6 @@ import MkFolder from '@/components/MkFolder.vue';
 import MkKeyValue from '@/components/MkKeyValue.vue';
 import MkLink from '@/components/MkLink.vue';
 import MkInfo from '@/components/MkInfo.vue';
-import { donateCherryPick } from '@/utility/donate-cherrypick.js';
 
 const initStats = () => misskeyApi('stats', {});
 </script>
