fae24f9068bcec289940fae98c9aa6315ea5f41c
tree-townhouse <tree@townhouse.work>
2026-02-26 01:46:50 +0900

    fix

--- packages/frontend/src/boot/main-boot.ts
+++ packages/frontend/src/boot/main-boot.ts
@@ -309,13 +309,6 @@ export async function mainBoot() {
 			}
 		}
 
-		const modifiedVersionMustProminentlyOfferInAgplV3Section13Read = miLocalStorage.getItem('modifiedVersionMustProminentlyOfferInAgplV3Section13Read');
-		if (modifiedVersionMustProminentlyOfferInAgplV3Section13Read !== 'true' && instance.repositoryUrl !== 'https://github.com/kokonect-link/cherrypick') {
-			const { dispose } = popup(defineAsyncComponent(() => import('@/components/MkSourceCodeAvailablePopup.vue')), {}, {
-				closed: () => dispose(),
-			});
-		}
-
 		if (store.s.realtimeMode) {
 			const stream = useStream();
 
