894028b279edcb499233cfbd94883ea8d7edfa7e
tree-townhouse <tree@townhouse.work>
2026-02-27 18:11:17 +0900

    feat: 후원 메시지 삭제

--- packages/frontend/src/boot/main-boot.ts
+++ packages/frontend/src/boot/main-boot.ts
@@ -299,16 +299,6 @@ export async function mainBoot() {
 		}
 		miLocalStorage.setItem('lastUsed', Date.now().toString());
 
-		const latestDonationInfoShownAt = miLocalStorage.getItem('latestDonationInfoShownAt');
-		const neverShowDonationInfo = miLocalStorage.getItem('neverShowDonationInfo');
-		if (neverShowDonationInfo !== 'true' && (createdAt.getTime() < (Date.now() - (1000 * 60 * 60 * 24 * 3))) && !window.location.pathname.startsWith('/miauth')) {
-			if (latestDonationInfoShownAt == null || (new Date(latestDonationInfoShownAt).getTime() < (Date.now() - (1000 * 60 * 60 * 24 * 30)))) {
-				const { dispose } = popup(defineAsyncComponent(() => import('@/components/MkDonation.vue')), {}, {
-					closed: () => dispose(),
-				});
-			}
-		}
-
 		if (store.s.realtimeMode) {
 			const stream = useStream();
 
