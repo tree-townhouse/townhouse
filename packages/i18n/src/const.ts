b5de3c5f4ad49609729c32d2a47b067d16176fbd
tree-townhouse <tree@townhouse.work>
2026-03-04 15:46:21 +0900

    feat: add channel moderation features and improve localization for Japanese and English

--- packages/i18n/src/const.ts
+++ packages/i18n/src/const.ts
@@ -4,34 +4,9 @@
  */
 
 export const languages = [
-	'ar-SA',
-	'ca-ES',
-	'cs-CZ',
-	'da-DK',
-	'de-DE',
 	'en-US',
-	'es-ES',
-	'fr-FR',
-	'id-ID',
-	'it-IT',
 	'ja-JP',
-	'ja-KS',
-	'kab-KAB',
-	'kn-IN',
 	'ko-KR',
-	'nl-NL',
-	'no-NO',
-	'pl-PL',
-	'pt-PT',
-	'ru-RU',
-	'sk-SK',
-	'th-TH',
-	'tr-TR',
-	'ug-CN',
-	'uk-UA',
-	'vi-VN',
-	'zh-CN',
-	'zh-TW',
 ] as const;
 
 export const primaries = {
